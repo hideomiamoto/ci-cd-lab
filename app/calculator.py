c905f6726b41b7f210bb2fad701a431a40b73d9b
Hideo Miamoto <hideomiamoto@yandex.ru>
2026-07-08 23:02:45 +0700

    Initial CI/CD lab project

---
+++ app/calculator.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def divide(a: int, b: int) -> float:
+    if b == 0:
+        raise ValueError("Division by zero is not allowed")
+    return a / b
